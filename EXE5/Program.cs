e75818de90e8d37a0a033eb5d00104cc65fcdb20
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-12-21 16:14:29 +0700

    menambahkan class Node

--- EXE5/Program.cs
+++ EXE5/Program.cs
@@ -6,21 +6,9 @@ using System.Threading.Tasks;
 
 namespace EXE5
 {
-    class Queues
+    class Node
     {
-        int MUH, SYAFRIZAL, max = 5;
-        int[] queue_array = new int[5];
-        public Queues()
-        {
-            /*Intializing the values of the variables REAR amd FRONT to -1
-            * to indicate that the queue is intially empty. */
-            MUH = -1;
-            SYAFRIZAL = -1;
-        }
-        public void insert(int element)
-        {
 
-        }
     }
     
 }
